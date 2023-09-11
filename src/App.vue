eec1a24e5822044d317d38e6f43d6ef75e9e7836
Yizhou "Andi" Cui <howdy@andicui.xyz>
2023-09-11 18:05:09 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,151 @@
+<template>
+    <title v-if="id">Random Bingo Card #{{ id.toString().padStart(6, '0') }}</title>
+    <title v-else>Bingo Card Generator</title>
+    <main>
+        <h1><span class="title-name" contenteditable>_______'s</span> Bingo Card</h1>
+        <h2 v-if="id">Random Bingo Card #{{ id.toString().padStart(6, '0') }}</h2>
+        <BingoSheet v-if="selectedWords" :wordList="selectedWords" />
+    </main>
+    <nav>
+        <!-- <button @click="edit()">Edit</button> -->
+        <button @click="generate()" :disabled="!wordList">Generate {{ id ? "New" : "" }} Card</button>
+        <button @click="print()">Print</button>
+    </nav>
+</template>
+
+<script>
+import BingoSheet from "@/components/BingoSheet.vue"
+
+import "@/assets/fonts/fonts.css"
+
+export default {
+    name: 'BingoGenerator',
+    components: {
+        BingoSheet
+    },
+    data: function () {
+        return {
+            id: null,
+            wordList: null,
+            selectedWords: null
+        };
+    },
+    watch: {
+        id: function () {
+            // Make the seed id into get parameter
+            window.history.replaceState({}, "", `?id=${this.id}`);
+            this.selectedWords = this.generateSelectedWords();
+        }
+    },
+    created: async function () {
+        this.wordList = await this.readWordList();
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get("id");
+        if (id) {
+            this.id = parseInt(id);
+        }
+    },
+    methods: {
+        print: function () {
+            window.print();
+        },
+        generate: function () {
+            this.id = Math.floor(Math.random() * 1_000_000);
+        },
+        pseudoRandom: function (seed) {
+            // https://stackoverflow.com/a/19303725
+            var x = Math.sin(seed) * 10000;
+            return x - Math.floor(x);
+        },
+        readWordList: async function () {
+            const response = await fetch("/word-lists/BingoListApple2023Sept.txt");
+            const text = await response.text();
+            return text.split("\n");
+        },
+        popAtRandom: function (array, seed = null) {
+            const random = seed ? this.pseudoRandom(seed) : Math.random();
+            const index = Math.floor(random * array.length);
+            return array.splice(index, 1)[0];
+        },
+        generateSelectedWords: function () {
+            if (!this.wordList) {
+                return null;
+            }
+            if (!this.id) {
+                return null;
+            }
+            const wordList = [...this.wordList];
+            const selectedWords = [];
+            for (let i = 0; i < 25; i++) {
+                const seed = this.id + i;
+                const word = this.popAtRandom(wordList, seed);
+                selectedWords.push(word);
+            }
+            return selectedWords;
+        },
+    }
+}
+
+</script>
+
+<style>
+:root {
+    font-family: 'IBM Plex Sans', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+@media print {
+    @page {
+        /* Prints on A4 with 1.5in margin */
+        width: 210mm;
+        height: 297mm;
+        margin: 1.5in;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+    }
+
+    nav {
+        display: none !important;
+    }
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    /* min-height: 100vh; */
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-family: 'Bitter Pro', 'IBM Plex Serif', Georgia, serif;
+    font-weight: 900;
+    margin: 0;
+}
+
+main {
+    margin-bottom: 12pt;
+}
+
+nav {
+    display: flex;
+    gap: 12pt;
+    align-items: center;
+    justify-content: center;
+}
+
+nav button {
+    font-size: 16pt;
+    font-family: 'IBM Plex Sans', Helvetica, Arial, sans-serif;
+    font-weight: 400;
+}
+</style>
---
+++ src/components/BingoSheet.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="sheet" v-if="squares" :class="{ 'bingo': this.bingo }">
+        <div class="square" v-for="sqr of squares" :key="sqr.id"
+            :class="{ 'free': sqr.isFreeSquare, 'checked': sqr.checked, 'winning-square': this.bingo && this.bingo.includes(sqr.id) }"
+            @click="toggleSquare(sqr.id)">{{ !sqr.isFreeSquare ? sqr.text : "" }}</div>
+    </div>
+</template>
+<style scoped>
+@media print {
+    .square {
+        -webkit-print-color-adjust: exact;
+        color-adjust: exact;
+        print-color-adjust: exact;
+    }
+}
+
+.sheet {
+    display: grid;
+    grid-template-columns: repeat(5, 96pt);
+    /* Load this from size variable */
+    margin: auto auto;
+}
+
+.sheet.bingo {}
+
+.square {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+    padding: 8pt;
+
+    border: 1pt solid black;
+    box-sizing: border-box;
+
+    width: 96pt;
+    aspect-ratio: 1;
+}
+
+.square.free {
+    background: url(@/assets/icons/free-square.svg) no-repeat center center / 64pt;
+}
+
+.square.checked:not(.free) {
+    /* overlay a cross */
+    background: url(@/assets/icons/check.svg) no-repeat center center;
+    background-size: 64pt;
+}
+
+.square.checked.winning-square {
+    background: none;
+    background-color: black;
+    color: white;
+}
+
+.square.winning-square.free {
+    background: url(@/assets/icons/free-square-white.svg) no-repeat center center / 64pt;
+    background-color: black;
+    color: white;
+}
+</style>
+<script>
+export default {
+    name: 'BingoSheet',
+    props: {
+        size: {
+            type: Number,
+            default: 5
+        },
+        freeSquare: {
+            type: Boolean,
+            default: true
+        },
+        wordList: {
+            type: Array
+        }
+    },
+    computed: {
+        totalSquares() {
+            return this.size * this.size
+        },
+        idxFreeSquare() {
+            // The free square is always in the middle of the board, none if the board is even
+            return this.freeSquare && this.size % 2 === 1 ? Math.floor(this.totalSquares / 2) : -1
+        },
+        bingo() {
+            if (!this.squares) {
+                return null
+            }
+            // A bingo is when a row, column, or diagonal has all squares checked
+            // Return all the ids of the squares that are part of the bingo
+            // If there are multiple bingos, return all of them
+            // If there are no bingos, return null
+
+            let winningSquares = []
+
+            // Check rows
+            for (let i = 0; i < this.size; i++) {
+                let row = this.squares.slice(i * this.size, (i + 1) * this.size)
+                if (row.every(sqr => sqr.checked)) {
+                    winningSquares = winningSquares.concat(row.map(sqr => sqr.id))
+                }
+            }
+
+            // Check columns
+            for (let i = 0; i < this.size; i++) {
+                let col = this.squares.filter((_, idx) => idx % this.size === i)
+                if (col.every(sqr => sqr.checked)) {
+                    winningSquares = winningSquares.concat(col.map(sqr => sqr.id))
+                }
+            }
+
+            // Check diagonals
+            let diag1 = this.squares.filter((_, idx) => idx % (this.size + 1) === 0)
+            let diag2 = this.squares.filter((_, idx) => idx % (this.size - 1) === 0 && idx > 0 && idx < this.totalSquares - 1)
+            if (diag1.every(sqr => sqr.checked)) {
+                winningSquares = winningSquares.concat(diag1.map(sqr => sqr.id))
+            } else if (diag2.every(sqr => sqr.checked)) {
+                winningSquares = winningSquares.concat(diag2.map(sqr => sqr.id))
+            }
+
+            // Make sure there are no duplicates
+            winningSquares = [...new Set(winningSquares)]
+
+            return winningSquares.length > 0 ? winningSquares : null
+        }
+    },
+    watch: {
+        wordList() {
+            this.generateSquares()
+        }
+
+    },
+    data() {
+        return {
+            squares: null
+        }
+    },
+    mounted() {
+        this.generateSquares()
+    },
+    methods: {
+        toggleSquare(id) {
+            if (!this.squares || id >= this.squares.length || this.squares[id].isFreeSquare) {
+                return
+            }
+            // Toggle the checked state of a square
+            this.squares[id].checked = !this.squares[id].checked
+        },
+        generateSquares() {
+            const squares = []
+            for (let i = 0; i < this.totalSquares; i++) {
+                squares.push({
+                    id: i,
+                    text: this.wordList ? this.wordList[i] : "",
+                    isFreeSquare: this.freeSquare && i === this.idxFreeSquare,
+                    checked: i === this.idxFreeSquare // The free square is always checked
+                })
+            }
+            this.squares = squares
+        }
+    }
+}
+</script>

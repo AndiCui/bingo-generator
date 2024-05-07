5afc32246f2facfcbebbd990029cf62175330059
Yizhou "Andi" Cui <howdy@andicui.xyz>
2024-05-07 12:43:58 +0200

    Wordlist Update

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,7 @@ export default {
             return x - Math.floor(x);
         },
         readWordList: async function () {
-            let url = "./word-lists/BingoListGalaxyUnpacked2024.txt";
+            let url = "./word-lists/BingoListApple2024May.txt";
             const response = await fetch(url);
             const text = await response.text();
             return text.split("\n");
